7e30f847505f4261e89bc8ce675e8acad5ac0f03
ommjyoti12 <ommjyotiputhal61@gmail.com>
2025-05-01 09:46:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+let input=document.querySelector("input");
+let button=document.querySelector("button");
+let ul =document.querySelector("ol");
+button.addEventListener("click",function(){
+    let item=document.createElement("li");
+    item.innerText=input.value;
+    ul.appendChild(item);
+    input.value="";
+})
